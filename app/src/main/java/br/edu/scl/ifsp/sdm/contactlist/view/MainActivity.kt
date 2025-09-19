411fd4ba13c245ba60ea2953039c664c46b98660
July <julylemos1@hotmail.com>
2025-09-19 02:46:42 -0300

    Fase 2 - Adapter

--- app/src/main/java/br/edu/scl/ifsp/sdm/contactlist/view/MainActivity.kt
+++ app/src/main/java/br/edu/scl/ifsp/sdm/contactlist/view/MainActivity.kt
@@ -1,9 +1,17 @@
 package br.edu.scl.ifsp.sdm.contactlist.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.ArrayAdapter
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import br.edu.scl.ifsp.sdm.contactlist.R
+import br.edu.scl.ifsp.sdm.contactlist.adapter.ContactAdapter
 import br.edu.scl.ifsp.sdm.contactlist.databinding.ActivityMainBinding
+import br.edu.scl.ifsp.sdm.contactlist.model.Constant.EXTRA_CONTACT
 import br.edu.scl.ifsp.sdm.contactlist.model.Contact
 
 class MainActivity : AppCompatActivity() {
@@ -15,30 +23,67 @@ class MainActivity : AppCompatActivity() {
     private val contactList: MutableList<Contact> = mutableListOf()
 
     // Adapter
-    private val contactAdapter: ArrayAdapter<String> by lazy {
-        ArrayAdapter(this, android.R.layout.simple_list_item_1, contactList.map{ it.toString() })
+    private val contactAdapter: ContactAdapter by lazy {
+        ContactAdapter(this, contactList)
     }
+
+    private lateinit var carl: ActivityResultLauncher<Intent>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(amb.root)
 
-        fillContacts()
+        setSupportActionBar(amb.toolbarIn.toolbar)
+        supportActionBar?.subtitle = getString(R.string.contact_list)
 
-        amb.contactsLv.adapter = contactAdapter
-    }
+        carl = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == RESULT_OK) {
+                    val contact = result.data?.getParcelableExtra<Contact>(EXTRA_CONTACT)
+                    contact?.also {
+                        if (contactList.any{ it.id == contact.id }) {
+                            //edição
+                        } else {
+                            contactList.add(contact)
+                        }
+                        contactAdapter.notifyDataSetChanged()
+                    }
+                }
+            }
 
-    private fun fillContacts() {
-        for (i in 1..50) {
-            contactList.add(
-                Contact(
-                    i,
-                    "Nome $i",
-                    "Endereço $i",
-                    "Telefone $i",
-                    "Email $i"
+                fillContacts()
+
+                amb.contactsLv.adapter = contactAdapter
+            }
+
+        override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+            menuInflater.inflate(R.menu.menu_main, menu)
+            return true
+        }
+
+        override fun onOptionsItemSelected(item: MenuItem): Boolean {
+            return when (item.itemId) {
+                R.id.addContactMi -> {
+                    carl.launch(Intent(this, ContactActivity::class.java))
+                    true
+                }
+
+                else -> {
+                    false
+                }
+            }
+        }
+
+        private fun fillContacts() {
+            for (i in 1..10) {
+                contactList.add(
+                    Contact(
+                        i,
+                        "Nome $i",
+                        "Endereço $i",
+                        "Telefone $i",
+                        "Email $i"
+                    )
                 )
-            )
+            }
         }
     }
-}
